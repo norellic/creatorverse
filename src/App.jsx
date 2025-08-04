aaa21b0005e4b47b9834ff03c5f884b2b61202aa
ongarrick <ong3@duke.edu>
2025-08-03 22:50:49 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import { supabase } from './client'
+import Post from '../components/Post.jsx'
+
+function App() {
+
+  //
+
+  //routes
+
+  return (
+    <>
+      <div className="navbar">
+        <h3 className="navbar_text">Home</h3>
+      </div>
+
+      <div className="whole_page">
+        <div className="post_feed">
+          {/*  */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
